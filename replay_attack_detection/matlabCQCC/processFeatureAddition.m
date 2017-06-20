6d26521495f3bff31c896b0cde673ec5661b8756
Jacob <jacob.filin@gmail.com>
2017-06-20 21:48:47 +0700

    Add files via upload

---
+++ replay_attack_detection/matlabCQCC/processFeatureAddition.m
@@ -0,0 +1,19 @@
+clear; close all; clc;
+
+dir = './';
+
+genuineFeatureCellDev = addNewFeature(dir,'genuineFeatureCellDev','snrOriginalDEV', 'excessOriginalDEV','sigmaOriginalDEV' );
+save ('./cqcc_and_3feature/genuineFeatureCellDev.mat', 'genuineFeatureCellDev','-v7.3');
+
+genuineFeatureCellTrain = addNewFeature(dir,'genuineFeatureCellTrain','snrOriginalTRAIN', 'excessOriginalTRAIN','sigmaOriginalTRAIN' );
+save ('./cqcc_and_3feature/genuineFeatureCellTrain.mat', 'genuineFeatureCellDev','-v7.3');
+
+spoofFeatureCellDev = addNewFeature(dir,'spoofFeatureCellDev','snrSpoofDEV', 'excessSpoofDEV','sigmaSpoofDEV' );
+save ('./cqcc_and_3feature/spoofFeatureCellDev.mat', 'spoofFeatureCellDev','-v7.3');
+
+spoofFeatureCellTrain = addNewFeature(dir,'genuineFeatureCellDev','snrSpoofTRAIN', 'excessSpoofTRAIN','sigmaSpoofTRAIN' );
+save ('./cqcc_and_3feature/spoofFeatureCellTrain.mat', 'spoofFeatureCellTrain','-v7.3');
+
+
+evaluationFeature = addNewFeature(dir,'evaluationFeature','snrEVA', 'excessEVA','sigmaEVA' );
+save ('./cqcc_and_3feature/evaluationFeature.mat', 'evaluationFeature','-v7.3');
